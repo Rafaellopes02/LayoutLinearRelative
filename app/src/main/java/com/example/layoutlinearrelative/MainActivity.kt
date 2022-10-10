ab4fac12c5b198f2ac4dda80ce4d956f67aafa05
Rafael Lopes <rafael.lopes@ipvc.pt>
2022-10-10 18:10:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/layoutlinearrelative/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layoutlinearrelative
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
